9da823200807f976ae01eccff102028f7fa29561
Ли Сергей <sergey.r.lee@gmail.com>
2022-02-20 17:19:21 +0300

    Решена только одна задача к ДЗ № 6.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int[] arr1 = {1, 2, 3, 1, 0};
-        if
+        int[] arr1 = {0, 2, 3, 4, 5, 2, 1, 0};
+        if (arr1.length < 3) {
+            System.out.println("Горы не получится.");
+            return;
+        }
         boolean mountain = (arr1[0] < arr1[1]);
+        if (!mountain) {
+            System.out.println("Гора не получается.");
+            return;
+        }
         int i = 1;
         for (; i <= arr1.length - 2; i++) {
             if (arr1[i] < arr1[i + 1]) {
@@ -18,7 +25,11 @@ public class Main {
             if (arr1[i] > arr1[i + 1]) {
                 continue;
             }
-            else break;
+            else {
+                System.out.println("Гора не получается.");
+                return;
+            }
         }
+        System.out.println("Ура! Гора получается.");
     }
 }
