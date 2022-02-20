1654267bdc97acad5a4ee70af28ff0940e80e72a
Ли Сергей <sergey.r.lee@gmail.com>
2022-02-20 16:51:50 +0300

    Дополнительное задание к ДЗ № 6 ещё не выполнено.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
+        int[] arr1 = {1, 2, 3, 1, 0};
+        if
+        boolean mountain = (arr1[0] < arr1[1]);
+        int i = 1;
+        for (; i <= arr1.length - 2; i++) {
+            if (arr1[i] < arr1[i + 1]) {
+                continue;
+            }
+            else break;
+        }
+        for (; i < arr1.length - 1; i++) {
+            if (arr1[i] > arr1[i + 1]) {
+                continue;
+            }
+            else break;
+        }
     }
 }
