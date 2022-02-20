202272282e64fad1cd99100370d66e608dfabcdc
Ли Сергей <sergey.r.lee@gmail.com>
2022-02-20 20:40:35 +0300

    Задача № 2 к ДЗ № 6 не получается.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -31,5 +33,26 @@ public class Main {
             }
         }
         System.out.println("Ура! Гора получается.");
+
+        System.out.println();
+        int[] arr21 = {4, 9, 5};
+        int[] arr22 = {9, 4, 9, 8, 4};
+        int arr23Length = arr21.length;
+        if (arr22.length < arr21.length) {
+            arr23Length = arr22.length;
+        }
+        int j = 0;
+        for (int k = 1; k < arr23Length - 1; k++) {
+            if (arr21[k] == arr22[k] || arr1[k] == arr22[k + 1] || arr21[k] == arr22[k-1]) {
+                int[] arr23 = new int[k];
+                arr23[j] = arr21[k];
+                j = j + 1;
+            }
+            else continue;
+        }
+        if (arr23 == null) {
+            System.out.println("В Ваших массивах нет пересечений.");
+        }
+        else System.out.println("Массив пересечений " + Arrays.toString(arr23));
     }
 }
